775c1a68a76d92fb50ba904e1e7e00a98ca0e24f
Danilo Volles <danilo.volles@outlook.com>
2023-08-09 08:53:10 -0300

    Constraint Layout Practice

---
+++ app/src/main/java/com/example/dashboardui/ConstraintLayoutPractice2.kt
@@ -0,0 +1,11 @@
+package com.example.dashboardui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintLayoutPractice2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout_practice2)
+    }
+}
